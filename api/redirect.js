904a4eba1b08af019023f4f63c1b4b2443a578a2
aungbobokyaw.com <141192688+aungbobokyaw124@users.noreply.github.com>
2026-09-05 04:08:09 +0630

    Change import syntax from ES6 to CommonJS

--- api/redirect.js
+++ api/redirect.js
@@ -1,4 +1,4 @@
-import { supabase } from '../lib/supabase.js'
+const { supabase } = require('../lib/supabase.js')
 
 export default async function handler(req, res) {
   const { slug } = req.query || {}
